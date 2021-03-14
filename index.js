cca4c0252f02040809ce4f88c7e38831d40145f2
AndreWeb <andzion105@gmail.com>
2021-03-14 03:41:23 +0200

    add pointer-events on btn to json click

--- index.js
+++ index.js
@@ -12,28 +12,91 @@ window.addEventListener('DOMContentLoaded', () => {
     btnReset = document.querySelector('#resetBtn');
 
   let mainSet = {},
+    setToJSON = {},
     localDB,
-    count = 0;
-    console.log(mainSet);
+    count;
+
+    upload.addEventListener('change', (e) => {
+      clearConditions();
+
+      try {
+        const upload = e.target.files[0];
+        const reader = new FileReader();
+        upload.value = '';
+        reader.addEventListener('load', (() => {
+          return (e) => {
+            mainSet = JSON.parse(e.target.result);
+            unpackObject(mainSet);
+            };
+          })(upload)
+        );
+        reader.readAsText(upload);
+      } catch (ex) {
+        console.log(ex);
+      }
+
+    });
+  
+    upload.addEventListener('click', () => {
+      upload.value = '';
+    });
     
-  (function toSelectDB() {
-    for (let i = 0; i < examplesDB.length; i++) {
-      examplesDB[i].addEventListener('click', (e) => {
-        let receivedPath = e.target.dataset.db;
-        createPathToDB(receivedPath);
-      });
+    (function toSelectDB() {
+      for (let i = 0; i < examplesDB.length; i++) {
+        examplesDB[i].addEventListener('click', (e) => {
+          let receivedPath = e.target.dataset.db;
+          createPathToDB(receivedPath);
+        });
+      }
+    })();
+
+    btnReset.addEventListener('click', () => {
+      clearConditions();
+      localStorage.clear();
+    });
+
+    btnToJSON.addEventListener('click', () => {
+      btnToJSON.classList.add('visited');
+      let json = JSON.stringify(mainSet);
+      if (Object.keys(mainSet).length > 0) {
+        addNoticeAfterConvert('filled');
+        localStorage.setItem('json', json);
+      } else {
+        addNoticeAfterConvert('empty');
+      }
+
+    });
+  
+    function addNoticeAfterConvert(option) {
+      let p = document.createElement('p');
+      p.classList.add('to-json-prompt');
+      if (option === 'filled') {
+        p.innerHTML = 'Local storage has updated';
+      } else {
+        p.innerHTML = 'Select some JSON above';
+        p.classList.add('error');
+      }
+  
+      setTimeout(() => {
+        saveBlock.appendChild(p);
+      }, 600);
+      setTimeout(() => {
+        saveBlock.removeChild(p);
+        btnToJSON.classList.remove('visited');
+      }, 2500);
     }
-  })();
+    
+    function createPathToDB(path) {
+      localDB = `/db/${path}.json`;
+      getDataFetch(localDB);
+    }
+    
+    async function getDataFetch(url) {
+      clearConditions();
 
-  function createPathToDB(path) {
-    localDB = `/db/${path}.json`;
-    getDataFetch(localDB);
-  }
-
-  async function getDataFetch(url) {
-    let response = await fetch(url);
-    mainSet = await response.json();
-    unpackObject(mainSet);
+      let response = await fetch(url);
+      mainSet = await response.json();
+      unpackObject(mainSet);
   }
 
   function unpackObject(data) {
@@ -51,17 +114,22 @@ window.addEventListener('DOMContentLoaded', () => {
   
   function renderIncomingData(k, v) {  
     incomingDataField.appendChild(list);
-    
     if (typeof k === 'object') {
       count++;
       list.innerHTML += `<h4>Set ${count}:</h4>`;
     } else {
-      list.innerHTML += `<li>${k}: ${v}</li>`;
-      
+      list.innerHTML += `<li>${k}: &nbsp;${v}</li>`;
     }
   }
 
-  function clearMainSet(obj) {
+  function clearConditions() {
+      count = 0;
+      list.innerHTML = '';
+      listRes.innerHTML = '';
+      deletePropsFromObj(mainSet)
+  }
+
+  function deletePropsFromObj(obj) {
     let props = Object.keys(obj);
     for (var i = 0; i < props.length; i++) {
       delete obj[props[i]];
@@ -96,7 +164,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     set.converted = Math.trunc(result * 100) / 100;
     renderResultData(set);
-    console.log(set);
   }
 
   function convertInMetres(init, amount) {
@@ -160,30 +227,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     return endedProp;
   }
+
 });
 
-// (function toSelectDB() {
-//   for (let i = 0; i < examplesDB.length; i++) {
-//     examplesDB[i].addEventListener('click', (e) => {
-//       let receivedPath = e.target.dataset.db;
-//       createPathToDB(receivedPath);
-//     });
-//   }
-// })();
-
-// function createPathToDB(path) {
-//   localDB = `/db/${path}.json`;
-//   getDataFetch(localDB);
-// }
-
-// const getDataFetch = async (url) => {
-//   const res = await fetch(url);
-
-//   if (!res.ok) {
-//     throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);
-//   }
-
-//   let data = await res.json();
-//   await createSetOfNumberArrays(data);
-//   renderIncomeData(data);
-// };
+// document.getElementById("input-field").value = "";
+//      this.uploadColors(e)
